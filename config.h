a22d523d53c35b3014f0545d3d7c062d2db31892
Luke Smith <luke@lukesmith.xyz>
2019-02-08 16:26:06 -0500

    reversion to default colors

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#458588", "#282828" },
-	[SchemeSel] = { "#282828", "#458588" },
-	[SchemeOut] = { "#282828", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
