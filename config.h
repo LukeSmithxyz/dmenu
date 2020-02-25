934fae8c0457d963992ec6442070a2b7c32d02af
Luke Smith <luke@lukesmith.xyz>
2020-02-25 16:49:59 -0500

    JoyPixels added

--- config.h
+++ config.h
@@ -4,7 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=14"
+	"monospace:size=14",
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
