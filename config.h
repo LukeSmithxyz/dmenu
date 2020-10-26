f4545c0e4e27616f99b799044c61e7c4479a40a3
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-10-26 04:54:24 +0300

    fuzzy search mode with -F flag, applied 'fuzzymatch' patch

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
