38add44c130637e47e107050272294ad6fd2c015
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-10-26 02:08:53 +0000

    fix for enabling fuzzy search with -F flag

--- dmenu.c
+++ dmenu.c
@@ -1008,7 +1008,7 @@ main(int argc, char *argv[])
 		else if (!strcmp(argv[i], "-f"))   /* grabs keyboard before reading stdin */
 			fast = 1;
 		else if (!strcmp(argv[i], "-F"))   /* grabs keyboard before reading stdin */
-			fuzzy = 0;
+			fuzzy = 1;
 		else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
 			fstrncmp = strncasecmp;
 			fstrstr = cistrstr;
