ab4967a0663c66003358f5a036ffb77a7df9bc3b
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-10-26 05:46:52 +0300

    little fix for case insensitive search

--- dmenu.c
+++ dmenu.c
@@ -180,7 +180,7 @@ drawhighlights(struct item *item, int x, int y, int maxw)
                           ? SchemeSelHighlight
                           : SchemeNormHighlight]);
        for (i = 0, highlight = item->text; *highlight && text[i];) {
-               if (*highlight == text[i]) {
+               if (!fstrncmp(&(*highlight), &text[i], 1)) {
                        /* get indentation */
                        c = *highlight;
                        *highlight = '\0';
