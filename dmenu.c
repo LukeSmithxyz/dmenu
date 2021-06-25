d98ad821c8a28550be1e8889f4ea65333203593d
anonymous <anonymous@somemailprovider.test>
2021-06-25 03:03:05 +0300

    ctrl+v ability

--- dmenu.c
+++ dmenu.c
@@ -565,6 +565,8 @@ keypress(XKeyEvent *ev)
 			break;
 		case XK_y: /* paste selection */
 		case XK_Y:
+		case XK_v:
+		case XK_V:
 			XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
 			                  utf8, utf8, win, CurrentTime);
 			return;
