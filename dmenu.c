90ae8b1c94e82c9dbb5c61b286f8c7b180f233e0
Crony Akatsuki <crolinuxgamer@gmail.com>
2022-07-21 17:18:56 +0200

    extended numpad support.

--- dmenu.c
+++ dmenu.c
@@ -429,9 +429,11 @@ keypress(XKeyEvent *ev)
 			                  utf8, utf8, win, CurrentTime);
 			return;
 		case XK_Left:
+        case XK_KP_Left:
 			movewordedge(-1);
 			goto draw;
 		case XK_Right:
+        case XK_KP_Right:
 			movewordedge(+1);
 			goto draw;
 		case XK_Return:
@@ -479,6 +481,7 @@ insert:
 		insert(NULL, nextrune(-1) - cursor);
 		break;
 	case XK_End:
+    case XK_KP_End:
 		if (text[cursor] != '\0') {
 			cursor = strlen(text);
 			break;
@@ -498,6 +501,7 @@ insert:
 		cleanup();
 		exit(1);
 	case XK_Home:
+    case XK_KP_Home:
 		if (sel == matches) {
 			cursor = 0;
 			break;
@@ -506,6 +510,7 @@ insert:
 		calcoffsets();
 		break;
 	case XK_Left:
+    case XK_KP_Left:
 		if (cursor > 0 && (!sel || !sel->left || lines > 0)) {
 			cursor = nextrune(-1);
 			break;
@@ -514,18 +519,21 @@ insert:
 			return;
 		/* fallthrough */
 	case XK_Up:
+    case XK_KP_Up:
 		if (sel && sel->left && (sel = sel->left)->right == curr) {
 			curr = prev;
 			calcoffsets();
 		}
 		break;
 	case XK_Next:
+    case XK_KP_Next:
 		if (!next)
 			return;
 		sel = curr = next;
 		calcoffsets();
 		break;
 	case XK_Prior:
+    case XK_KP_Prior:
 		if (!prev)
 			return;
 		sel = curr = prev;
@@ -542,6 +550,7 @@ insert:
 			sel->out = 1;
 		break;
 	case XK_Right:
+    case XK_KP_Right:
 		if (text[cursor] != '\0') {
 			cursor = nextrune(+1);
 			break;
@@ -550,6 +559,7 @@ insert:
 			return;
 		/* fallthrough */
 	case XK_Down:
+    case XK_KP_Down:
 		if (sel && sel->right && (sel = sel->right) == next) {
 			curr = next;
 			calcoffsets();
